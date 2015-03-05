eb965441a5d5940d47737150e7b08b021f008bc4
Roca <roca@RocadeMacBook-Pro.local>
2015-03-05 16:48:58 +0100

    modificaciones alumno 1

--- practicaGIT/GitAux.java
+++ practicaGIT/GitAux.java
@@ -1,5 +1,30 @@
-public ovid testGitAux(){
+package pkgGitHub;
 
-	//metodo rama testing 
-	System.out.println("Metodo de testing");
+/**
+ *
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <escribe tu nombre aqui>
+ * @author alumno: <escribe tu nombre aqui>
+ * 
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+        System.out.println("metodo 1");
+	System.out.println("modificado por alumno 1");//alumno1
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+    
+    public void metodoAlumno3(){
+        System.out.println("metodo 3");
+    }
+    
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+	//modificaciones alumno 1
+	System.out.println("Alumno 1 inserta esta sentencia");
+    }
 }
